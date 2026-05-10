6d9a63eb5b614d1767c6f2fd606a5fee68580bd4
JadePulong <christianjadepulong6@gmail.com>
2026-05-10 21:54:22 +0800

    Implement submit products

--- src/js/Products/RenderProducts.js
+++ src/js/Products/RenderProducts.js
@@ -20,7 +20,8 @@ export class RenderProducts extends ColumnElements{
             <td class="py-6 px-6"><span class="px-3 py-1 bg-[#f4f9e6] text-[#76a609] rounded-full text-[10px] font-black uppercase">${String(this.available).toUpperCase()}</span></td>
             <td class="py-6 px-6 text-center">
                 <button class="editImageButton text-[#76a609] font-black text-[10px] uppercase tracking-widest hover:underline" value="${this.id}">Image</button>
-                <button onclick="openEditProduct('edit')" class="editButton text-[#76a609] font-black text-[10px] uppercase tracking-widest hover:underline" value="${this.id}">Edit</button>
+                <button onclick="openEditProduct('edit', '${this.id}')" class="editButton text-[#76a609] font-black text-[10px] uppercase tracking-widest hover:underline" value="${this.id}">Edit</button>
+                 <button onclick="openDelete('products', '${this.id}')" class="editButton text-[#76a609] font-black text-[10px] uppercase tracking-widest hover:underline" value="${this.id}">Delete</button>
             </td>
         `,);
 
